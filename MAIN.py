d533aaf7f74a4c13b09c7805e564855a6939717e
TiDev00 <cisse.thierno@outlook.com>
2021-11-14 01:03:57 -0500

    Add tag root to a all files of a directory

--- MAIN.py
+++ MAIN.py
@@ -1,12 +1,24 @@
+import os
 
-myFile = "test.xml"
 
-with open(myFile, "r+") as file:
-    old = file.read()
-    file.seek(0)
-    file.write("<Collection>\n" + old)
-    file.close()
+# Function to add the root tag to all files in a directory
 
-with open(myFile, "a") as file:
-    file.write("</Collection>\n")
-    file.close()
+
+def root_tag(my_file_path):
+    directory = my_file_path
+
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+
+        if os.path.isfile(filepath):
+            with open(filepath, "r+") as file:
+                old = file.read()
+                file.seek(0)
+                file.write("<Collection>\n" + old)
+                file.close()
+
+            with open(filepath, "a") as file:
+                file.write("</Collection>\n")
+                file.close()
+
+root_tag("DossierTest")
