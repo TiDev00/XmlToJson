7476e2d6fc55d75c84869eb7e2a83a7c82350113
TiDev00 <cisse.thierno@outlook.com>
2021-11-14 03:24:19 -0500

    Create multiple json file from a directory

--- MAIN.py
+++ MAIN.py
@@ -1,6 +1,7 @@
 import os
 import xmltodict
 import json
+import codecs
 
 
 # Function to add the root tag to all files in a directory
@@ -11,13 +12,13 @@ def root_tag(my_filepath):
         filepath = os.path.join(directory, filename)
 
         if os.path.isfile(filepath):
-            with open(filepath, "r+") as file:
+            with codecs.open(filepath, "r+", encoding='ISO-8859-1') as file:
                 old = file.read()
                 file.seek(0)
                 file.write("<Collection>\n" + old)
                 file.close()
 
-            with open(filepath, "a") as file:
+            with codecs.open(filepath, "a", encoding='ISO-8859-1') as file:
                 file.write("</Collection>\n")
                 file.close()
 
@@ -30,14 +31,13 @@ def converter(my_filepath):
         filepath = os.path.join(directory, filename)
 
         if os.path.isfile(filepath):
-            with open(filepath, 'r') as file:
+            with codecs.open(filepath, 'r', encoding='ISO-8859-1') as file:
                 obj = xmltodict.parse(file.read())
                 file.close()
                 json_object = json.dumps(obj, indent=2)
 
-            with open(filepath.replace(".xml", ".json"), "w") as json_file:
+            with codecs.open(filepath.replace(".xml", ".json"), "w", encoding='ISO-8859-1') as json_file:
                 json_file.write(json_object)
                 json_file.close()
 
-converter("DossierTest")
-
+root_tag("docs")
